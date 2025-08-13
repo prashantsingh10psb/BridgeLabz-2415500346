3875a221fd9bfc3813bf21a16141c3686f87cfdf
prashantsingh10psb <prasant.kumar_cs.aiml24@gla.ac.in>
2025-08-13 16:27:09 +0530

    Create Volume_Cylinder.java

---
+++ ASSIGNMENT1/Volume_Cylinder.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Volume_Cylinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius of the cylinder: ");
+        double radius = scanner.nextDouble();
+        System.out.print("Enter the height of the cylinder: ");
+        double height = scanner.nextDouble();
+        double volume = Math.PI * radius * radius * height;
+        System.out.println("The volume of the cylinder is: " + volume);
+    }
+}
