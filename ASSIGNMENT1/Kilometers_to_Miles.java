8f9eff4c818843b97828befa6f462e440746afda
prashantsingh10psb <prasant.kumar_cs.aiml24@gla.ac.in>
2025-08-13 16:23:40 +0530

    Create Kilometers_to_Miles.java

---
+++ ASSIGNMENT1/Kilometers_to_Miles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Kilometers_to_Miles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter distance in kilometers: ");
+        double kilometers = scanner.nextDouble();
+        double miles = kilometers * 0.621371;
+        System.out.println("Distance in miles: " + miles);
+    }
+}
